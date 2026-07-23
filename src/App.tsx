fd4b2ecb01fcb1a17d10bf91d0160171b008a279
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-07-23 14:32:45 +0300

    feat: add routing with protected profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { Routes, Route } from 'react-router';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import Navbar from "@/components/Navbar.tsx";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
+
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/" element={
+                <>
+                    <Navbar />
+                    <HomePage />
+                </>
+            } />
+            <Route path="/profile" element={
+                <ProtectedRoute>
+                    <Navbar />
+                    <div>Profile Page</div>
+                </ProtectedRoute>
+            } />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
